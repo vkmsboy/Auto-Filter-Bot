d167d5ee1be2dc967499a1df8d98d87dd9e88a8d
Iam-The-Master <71867099+Iam-The-Master@users.noreply.github.com>
2021-08-18 11:20:38 +0000

    Update script.py

--- script.py
+++ script.py
@@ -3,14 +3,14 @@ class script(object):
 
     START_MSG = """<b>Hey {} !</b>
 
-<b><i>I'm An Advanced Filter bot Created For @MovieJunctionGroup 🔥
+<b><i>I'm An Auto Filter bot Created For @Moviejunction_Group 🔥
 
 Click /Help To Know More...! 🙊</b></i>"""
 
 
     HELP_MSG = """<b><i>No One Gonna Help You 🙊
 
-Created For @MovieJunctionGroup 😉</b></i>"""
+Created For @Moviejunction_Group 😉</b></i>"""
 
 
     ABOUT_MSG = """⭕️<b> My Name : @DianaPrincessBot</b>
