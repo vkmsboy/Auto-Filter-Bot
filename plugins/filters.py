5cf80941afa97d9637e15c240a876df4babedde9
vkmsboy <95024188+vkmsboy@users.noreply.github.com>
2022-02-28 23:17:35 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -36,7 +36,7 @@ async def filter(client: Bot, message: Message):
             msg_id = msg.message_id                     
             link = msg.link
             btn.append(
-               text=f"{file_name}" , url=f"{link}")
+               text=f"{file_name}" , url=f"{link}"
             )
 
         if not btn:
